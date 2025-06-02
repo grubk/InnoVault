36d3dac3d653d9232cb3c061b9c8a7534e93f868
grubk <grubert.kai@gmail.com>
2025-06-01 23:29:43 -0700

    download button for pdfs

--- views/formulabooklet.py
+++ views/formulabooklet.py
@@ -1,11 +1,14 @@
 import streamlit as st
-import base64
 
-def viewPDF(filename):
-    with open(filename, "rb") as f:
-        base64_pdf = base64.b64encode(f.read()).decode('utf-8')
-    pdf_display = f'<iframe src="data:application/pdf;base64,{base64_pdf}" width="1000" height="1400" type="application/pdf"></iframe>'
-    st.markdown(pdf_display, unsafe_allow_html=True)
+def downloadPDF(filepath):
+    with open(filepath, "rb") as f:
+        pdf_bytes = f.read()
+    st.download_button(
+        label="Download PDF",
+        data=pdf_bytes,
+        file_name=filepath.split("/")[-1],
+        mime="application/pdf"
+    )
 
 # Main
 options = ['MATH 101', 'PHYS 158', 'CHEM 154']
@@ -15,8 +18,13 @@ st.write(f"You selected: {selected_subject}")
 
 if selected_subject == 'MATH 101':
     st.write("Note: This booklet provided by the faculty shows very few of the formulas ACTUALLY used in the course, and this formula booklet was only usable during the 2025 examination. Formula booklets are not permitted during any of the two midterms.")
-    viewPDF("assets/math101formulasheet.pdf")
+    pdf_path = "assets/math101formulasheet.pdf"
+    downloadPDF(pdf_path)
+
 elif selected_subject == 'PHYS 158':
-    viewPDF("assets/phys158formulasheet.pdf")
+    pdf_path = "assets/phys158formulasheet.pdf"
+    downloadPDF(pdf_path)
+
 elif selected_subject == 'CHEM 154':
-    viewPDF("assets/chem154formulasheet.pdf")
+    pdf_path = "assets/chem154formulasheet.pdf"
+    downloadPDF(pdf_path)
